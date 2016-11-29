bc5dc162d26c921f02f543ef7e257805bf1cf726
MarkGrayson <mpal7487@740s-pl-193.local>
2016-11-29 13:50:35 -0700

    Started new project.

---
+++ src/stuff/model/Thing.java
@@ -0,0 +1,16 @@
+package stuff.model;
+
+public abstract class Thing implements DoesStuff
+{
+	private String words;
+	
+	public String getWords()
+	{
+		return words;
+	}
+	
+	public void setWords(String notWords)
+	{
+		this.words = notWords;
+	}
+}
---
+++ src/stuff/model/ThirdThing.java
@@ -0,0 +1,21 @@
+package stuff.model;
+
+public class ThirdThing extends SubThing implements OtherInterface
+{
+	public int [] stackOfNumbers()
+	{
+		int [] stuffInside = {1,3,4,5,6,7};
+		return stuffInside;
+	}
+	
+	public void uselessMethod()
+	{
+		
+	}
+	
+	public void otherUselessMethod(double someThingElse)
+	{
+		
+	}
+	
+}
